da2a01af9485d1161ed17dc2d17be02927498de4
Moaz <148714017+Moaz787@users.noreply.github.com>
2024-11-29 17:14:35 +0200

    d

--- src/users/users.model.js
+++ src/users/users.model.js
@@ -1,11 +1,25 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const usersSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  age: { type: String, required: true },
-});
+const usersSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    age: {
+      type: Number,
+      required: [true, "Age is required"],
+    },
+  },
+  { timestamps: true }
+);
 
 const usersModel = mongoose.model("users", usersSchema);
 
